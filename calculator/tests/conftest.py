3201d8a084f3fb4528e2c868b5611c64212c1f07
Daanish Hindustani <134811343+Daanish-Hindustani@users.noreply.github.com>
2025-02-17 18:47:57 -0600

    Delete calculator directory

--- calculator/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from src.main import app
-from src.equation import Equation
-
-@pytest.fixture(scope="session")
-def client():
-    client = TestClient(app)
-    yield client
